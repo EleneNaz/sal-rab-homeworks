c6e39ba124ec0e691ab1e52d13873ed61bf65126
EleneNaz <aar1900@yandex.ru>
2022-04-13 22:19:27 +0500

    #bugfix task #2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,10 +9,11 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-if  (productsSum == 0) {shippingSum = 0};
+
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-if (productsSum >= freeShippingMinSum) {shippingSum = 0};
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {shippingSum = 0};
+      
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 if (productsSum > 0 && productsSum < freeShippingMinSum) {shippingSum = shippingPrice};
@@ -29,11 +30,11 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-let discountSum;
+   
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-if (productsSum >= discountMinSum ) {discountSum = discountPart / 100 * productsSum } else {discountSum = 0};
+    let discountSum = productsSum >= discountMinSum ? discountPart / 100 * productsSum : 0;
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -58,8 +59,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-    let freeShipping;
-    (shippingSum == 0) ? freeShipping = true : freeShipping = false;
+    let freeShipping = shippingSum == 0;
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
